2be40492bd191d8174b2120cadd74a8f264558cc
Tim.Riemann <tim.riemann@traxon-ecue.com>
2023-11-30 10:30:42 +0100

    add size constants for data types, fix bug in add_temperature, add test for add_temperature

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,20 @@ const LPP_BAROMETRIC_PRESSURE: u8 = 115;     // 2 bytes 0.1 hPa Unsigned
 const LPP_GYROMETER: u8 =           134;     // 2 bytes per axis, 0.01 °/s
 const LPP_GPS: u8 =                 136;     // 3 byte lon/lat 0.0001 °, 3 bytes alt 0.01 meter
 
+// Data ID + Data Type + Data Size
+const LPP_DIGITAL_INPUT_SIZE: usize =       3;       // 1 byte
+const LPP_DIGITAL_OUTPUT_SIZE: usize =      3;       // 1 byte
+const LPP_ANALOG_INPUT_SIZE: usize =        4;       // 2 bytes, 0.01 signed
+const LPP_ANALOG_OUTPUT_SIZE: usize =       4;       // 2 bytes, 0.01 signed
+const LPP_LUMINOSITY_SIZE: usize =          4;       // 2 bytes, 1 lux unsigned
+const LPP_PRESENCE_SIZE: usize =            3;       // 1 byte, 1
+const LPP_TEMPERATURE_SIZE: usize =         4;       // 2 bytes, 0.1°C signed
+const LPP_RELATIVE_HUMIDITY_SIZE: usize =   3;       // 1 byte, 0.5% unsigned
+const LPP_ACCELEROMETER_SIZE: usize =       8;       // 2 bytes per axis, 0.001G
+const LPP_BAROMETRIC_PRESSURE_SIZE: usize = 4;       // 2 bytes 0.1 hPa Unsigned
+const LPP_GYROMETER_SIZE: usize =           8;       // 2 bytes per axis, 0.01 °/s
+const LPP_GPS_SIZE: usize =                 11;      // 3 byte lon/lat 0.0001 °, 3 bytes alt 0.01 meter
+
 struct CayenneLPP<'a> {
     buffer: &'a mut [u8],
     index: usize
@@ -30,19 +44,12 @@ impl<'a> CayenneLPP<'a> {
         let temperature: u16 = (celsius * 10.0) as u16;
         let temperature_bytes = temperature.to_be_bytes();
 
-        self.buffer[{ self.index += 1; self.index }] = channel;
+        self.buffer[self.index] = channel;
         self.buffer[{ self.index += 1; self.index }] = LPP_TEMPERATURE;
         self.buffer[{ self.index += 1; self.index }] = temperature_bytes[0];
         self.buffer[{ self.index += 1; self.index }] = temperature_bytes[1];
+        self.index += 1;
     }
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    let mut buffer: [u8; 100] = [0; 100];
-    let mut lpp = CayenneLPP::create(&mut buffer);
-    lpp.add_temperature(0, 32.3);
-
-    left + right
 }
 
 #[cfg(test)]
@@ -50,8 +57,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn add_temperature() {
+        let mut buffer: [u8; 2 * LPP_TEMPERATURE_SIZE] = [0; 2 * LPP_TEMPERATURE_SIZE];
+        let mut lpp = CayenneLPP::create(&mut buffer);
+        
+        lpp.add_temperature(3, 27.2);
+        lpp.add_temperature(5, 25.5);
+
+        let expected_bytes: [u8; 8] = [0x03, 0x67, 0x01, 0x10, 0x05, 0x67, 0x00, 0xFF];
+        assert_eq!(expected_bytes, buffer);
+        
     }
 }
